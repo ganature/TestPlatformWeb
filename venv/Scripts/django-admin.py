8df6e9dcb80dd1428fe26824d9f0ffc12635ff1e
ganature <498857839@qq.com>
2019-02-15 19:42:11 +0800

    .

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\49885\PycharmProjects\TestPlatformWeb\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
