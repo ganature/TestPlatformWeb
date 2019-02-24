fbb278d11ec9895b1bebb32c846751fab34e2e7e
ganature <498857839@qq.com>
2019-02-24 23:21:02 +0800

    修复bug

--- apps/project/api_views.py
+++ apps/project/api_views.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import logging
 
 from rest_framework import status
 from rest_framework.response import Response
@@ -12,6 +13,8 @@ from rest_framework.pagination import PageNumberPagination
 from collections import OrderedDict
 
 from apps.project.models import Project
+from apps.users.models import UserProfile
+from apps.project.forms import ProjectForm
 from apps.project.serializers import ProjectSerializers
 from TestPlatformWeb.settings import BASE_DIR
 
@@ -41,26 +44,87 @@ class ProjectListView(generics.ListAPIView):
     pagination_class = ProjectPagination
 
 
+class ProjectAddView(APIView):
+    """
+    项目新增页面
+    """
+
+    def get_object(self, pk):
+        try:
+            return Project.objects.get(pk=pk)
+        except Exception as e:
+            pass
+
+    def post(self, request):
+        print(request.POST)
+        project = Project.objects.all()
+        serializer = ProjectSerializers(project, data=request.POST)
+
+        if serializer.is_valid():
+            serializer.save()
+            data = {
+                "error_code": 0,
+                "error_msg": "ok",
+                "data": serializer.data,
+                }
+            return Response(data=data)
+        else:
+            data = {
+                "data": serializer.errors,
+                }
+            return Response(data=data, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ProjectEditView(APIView):
+    """
+    项目编辑
+    """
+
+    def get_object(self, pk):
+        try:
+            return Project.objects.get(pk=pk)
+        except Exception as e:
+            pass
+
+    def get(self, request):
+        pk = request.GET['pk']
+        project = self.get_object(pk)
+        serializer = ProjectSerializers(project)
+        data = {
+            "error_code": 0,
+            "error_msg": "ok",
+            "data": serializer.data,
+            }
+        return Response(data)
+
+    def put(self, request, pk):
+        project = self.get_object(pk)
+        serializer = ProjectSerializers(project, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 class ProjectSyncView(APIView):
 
     def post(self, request):
         id = request.POST['project_id']
         print('ProjectSync request id:{}'.format(id))
         project = Project.objects.get(id=id)
-        project_path=BASE_DIR+'/project/'+project.name
+        project_path = BASE_DIR + '/project/' + project.name
         print("project_path : {}".format(project_path))
         print(project_path)
         try:
-            if os.path.exists(project_path) :
+            if os.path.exists(project_path):
                 os.chdir(project_path)
                 os.system("git pull")
             else:
                 os.makedirs(project_path)
-                print("git clone {} {}".format(project.url,project_path))
-                os.system("git clone {} {}".format(project.url,project_path))
+                print("git clone {} {}".format(project.url, project_path))
+                os.system("git clone {} {}".format(project.url, project_path))
                 # os.system("git clone {}".format(project.url))
             return Response(OrderedDict([("status_code", 200)]))
         except Exception as e:
             print(e)
-            return Response(("status_code",100),status =status.HTTP_400_BAD_REQUEST )
-
+            return Response(("status_code", 100), status=status.HTTP_400_BAD_REQUEST)
--- apps/project/urls.py
+++ apps/project/urls.py
@@ -2,16 +2,16 @@
 # -*- coding: utf-8 -*-
 from django.urls import path
 
-from apps.project.views import ProjectAddView, ProjectEditView,  ProjectView
-from apps.project.api_views import ProjectListView,ProjectSyncView
+from apps.project.views import ProjectView
+from apps.project.api_views import ProjectListView, ProjectSyncView, ProjectEditView, ProjectAddView
 
 app_name = 'project'
 urlpatterns = [
-            path('list/', ProjectView.as_view(), name='project_list'),
+    path('list/', ProjectView.as_view(), name='project_list'),
 
-            path('add/', ProjectAddView.as_view(), name='project_add'),
-            path('edit/<int:id>/', ProjectEditView.as_view(), name='project_detail'),
-            path('sync/', ProjectSyncView.as_view(), name='project_sync'),
-            # api路由配置
-            path('api/list', ProjectListView.as_view(), name='project_list_json'),
+    path('add/', ProjectAddView.as_view(), name='project_add'),
+    path('edit/', ProjectEditView.as_view(), name='project_detail'),
+    path('sync/', ProjectSyncView.as_view(), name='project_sync'),
+    # api路由配置
+    path('api/list', ProjectListView.as_view(), name='project_list_json'),
     ]
